a7f1678297de3237416131c097b31480094fde4e
Mr Robot <mrrobotcodedev@gmail.com>
2025-08-04 13:58:45 -0300

    style(empty-state): improves the appearance of the empty state

--- js/modules/Icons.js
+++ js/modules/Icons.js
@@ -86,9 +86,7 @@ const Icons = (() => {
     "calendar-sync":
       '<path d="M11 10v4h4"/><path d="m11 14 1.535-1.605a5 5 0 0 1 8 1.5"/><path d="M16 2v4"/><path d="m21 18-1.535 1.605a5 5 0 0 1-8-1.5"/><path d="M21 22v-4h-4"/><path d="M21 8.5V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h4.3"/><path d="M3 10h4"/><path d="M8 2v4"/>',
     x: '<line x1="18" y1="6" x2="6" y2="18"/><line x1="6" y1="6" x2="18" y2="18"/>',
-    "check-circle": '<path d="m9 12 2 2 4-4"/><circle cx="12" cy="12" r="9"/>',
-    "calendar-x":
-      '<rect x="3" y="4" width="18" height="18" rx="2" ry="2"/><line x1="16" y1="2" x2="16" y2="6"/><line x1="8" y1="2" x2="8" y2="6"/><line x1="3" y1="10" x2="21" y2="10"/><line x1="15" y1="14" x2="9" y2="20"/><line x1="9" y1="14" x2="15" y2="20"/>'
+    "check-circle": '<path d="m9 12 2 2 4-4"/><circle cx="12" cy="12" r="9"/>'
   };
 
   const getIconData = name => {
