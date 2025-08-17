932e46dac8bb311315833b72ab4c42e14d0a6135
Mr Robot <mrrobotcodedev@gmail.com>
2025-08-17 13:34:21 -0300

    feat: implements stack to manage and navigate between modals

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,19 +1,38 @@
-const Modal = (env => {
+const Modal = (() => {
+  const modalStack = [];
+
   function toggleModal(modalElement, show) {
     modalElement.classList.toggle("show", show);
   }
 
   function show(modalElement) {
+    if (!modalStack.includes(modalElement)) {
+      modalStack.push(modalElement);
+    }
     toggleModal(modalElement, true);
   }
 
-  function hide(modalElement, goBack) {
+  function hide(modalElement) {
     toggleModal(modalElement, false);
-    goBack && env.navigate(-1);
+
+    const index = modalStack.indexOf(modalElement);
+    if (index > -1) {
+      modalStack.splice(index, 1);
+    }
+  }
+
+  function goBack() {
+    if (modalStack.length > 0) {
+      const currentModal = modalStack.pop();
+      toggleModal(currentModal, false);
+      return true;
+    }
+    return false;
   }
 
   return {
     show,
-    hide
+    hide,
+    goBack
   };
-})(currentEnvironment);
+})();
